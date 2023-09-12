dc2f7ed91706afbe36773db8d20fb60fa72d67b1
Guido González <guiagugon@gmail.com>
2023-09-12 15:48:15 -0300

    Comitié todo. PRIMER COMMIT

---
+++ src/practica23/g007/Practica23G007.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica23.g007;
+
+/**
+ *
+ * @author gabri
+ */
+public class Practica23G007 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
